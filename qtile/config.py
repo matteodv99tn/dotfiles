96331eba91c51b001a22d97e91f343220aec4fb5
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-04-26 14:12:00 +0200

    Added qtile config

---
+++ qtile/config.py
@@ -0,0 +1,337 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+import subprocess
+import random
+import os
+from libqtile import hook
+
+from catppuccin import Flavour
+my_flavour = Flavour.frappe()
+
+N_MONITORS = int(subprocess.Popen(
+        "xrandr --listmonitors | wc -l",
+        stdout=subprocess.PIPE, 
+        shell=True
+        ).communicate()[0]) - 1
+
+COLORS = [
+        [my_flavour.base.hex, my_flavour.base.hex],
+        [my_flavour.text.hex, my_flavour.text.hex],
+        [my_flavour.mauve.hex, my_flavour.mauve.hex],
+        [my_flavour.blue.hex, my_flavour.blue.hex],
+        [my_flavour.teal.hex, my_flavour.teal.hex],
+        [my_flavour.subtext1.hex, my_flavour.subtext1.hex],
+        [my_flavour.subtext0.hex, my_flavour.subtext0.hex],
+        [my_flavour.overlay2.hex, my_flavour.overlay2.hex],
+        [my_flavour.overlay1.hex, my_flavour.overlay1.hex],
+        [my_flavour.overlay0.hex, my_flavour.overlay0.hex],
+        [my_flavour.surface2.hex, my_flavour.surface2.hex],
+        [my_flavour.surface1.hex, my_flavour.surface1.hex],
+        [my_flavour.surface0.hex, my_flavour.surface0.hex],
+        ]
+
+
+@hook.subscribe.startup
+def autostart():
+    # subprocess.run("picom &")
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
+    subprocess.run([home], stdin=None, stdout=None, stderr=None, close_fds=True)
+    lazy.spawn("thunderbird")
+
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "f", lazy.toggle_fullscreen()),
+]
+
+groups = [Group(i) for i in "123456789"]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+layouts = [
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2, margin=8),
+    # layout.MonadTall(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2, margin=8),
+    layout.Max(margin=[25, 50, 25, 400]),
+    layout.Max(margin=10),
+    layout.Zoomy(columnwidth=600, margin=[30, 40, 30, 40]),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+]
+
+widget_defaults = dict(
+    font="JetBrains Mono NL Medium",
+    fontsize=13,
+    padding=3,
+    foreground=COLORS[1]
+)
+extension_defaults = widget_defaults.copy()
+
+
+# https://raw.githubusercontent.com/tuffgniuz/qtile/master/unicodes.py
+LEFT_ARROW_UC = '\uE0B2'
+RIGHT_ARROW_UC = '\u25e2'
+ARROW_SIZE = 24
+
+def left_arrow(left_color=COLORS[0], right_color=COLORS[1]):
+    return widget.TextBox(
+        text=LEFT_ARROW_UC,
+        padding=0,
+        fontsize=ARROW_SIZE,
+        background=left_color,
+        foreground=right_color)
+
+def right_arrow(left_color=COLORS[1], right_color=COLORS[0]):
+    return widget.TextBox(
+        # text=RIGHT_ARROW_UC,
+        # background=right_color,
+        # foreground=left_color,
+        text = LEFT_ARROW_UC,
+        background=left_color,
+        foreground=right_color,
+        padding=0,
+        fontsize=ARROW_SIZE,
+        )
+
+
+def init_main_widget_list():
+    widgets = [
+            widget.GroupBox(
+                highlight_method = "line",
+                highlight_color = COLORS[-2],
+                rounded = True,
+                foreground = COLORS[1],
+                active = COLORS[-7],
+                inactive = COLORS[-1],
+                this_current_screen_border = COLORS[3],
+                other_current_screen_border = COLORS[1]
+            ),
+            right_arrow(COLORS[0], my_flavour.red.hex),
+            widget.CurrentLayout(foreground=COLORS[0], background=my_flavour.red.hex),
+            right_arrow(my_flavour.red.hex, my_flavour.blue.hex),
+            widget.Prompt(foreground=COLORS[0], background=my_flavour.blue.hex),
+            right_arrow(my_flavour.blue.hex, my_flavour.yellow.hex),
+            widget.WindowName(foreground=COLORS[0], background=my_flavour.yellow.hex),
+            # widget.Chord(
+            #     chords_colors={
+            #         "launch": ("#ff0000", "#ffffff"),
+            #         },
+            #     name_transform=lambda name: name.upper(),
+            #     ),
+            # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+            # widget.StatusNotifier(),
+            left_arrow(my_flavour.yellow.hex, COLORS[0]),
+            widget.Systray(),
+            left_arrow(COLORS[0], my_flavour.blue.hex),
+            widget.Battery(fmt="↯{} ", format="{percent:2.0%}", foreground=COLORS[0], background=my_flavour.blue.hex),
+            left_arrow(my_flavour.blue.hex, my_flavour.yellow.hex), 
+            widget.Clock(format="%Y-%m-%d %a %I:%M %p", background=my_flavour.yellow.hex, foreground = COLORS[0]),
+            left_arrow(my_flavour.yellow.hex, my_flavour.red.hex),
+            widget.QuickExit(foreground = COLORS[0], background=my_flavour.red.hex)
+            ]
+    return widgets
+
+
+def init_secondary_widget_list():
+    widgets = [
+            widget.CurrentLayout(foreground=COLORS[1]),
+            widget.GroupBox(
+                highlight_method = "line",
+                highlight_color = COLORS[-2],
+                rounded = True,
+                foreground = COLORS[1],
+                active = COLORS[-7],
+                inactive = COLORS[-1],
+                this_current_screen_border = COLORS[3],
+                other_current_screen_border = COLORS[5]
+                ),
+            widget.WindowName(),
+            widget.Chord(
+                chords_colors={
+                    "launch": ("#ff0000", "#ffffff"),
+                    },
+                name_transform=lambda name: name.upper(),
+                ),
+            left_arrow(COLORS[0], COLORS[3]),
+            widget.Clock(format="%Y-%m-%d %a %I:%M %p", background=COLORS[3]),
+            ]
+    return widgets
+
+widgets_monitor_2 = init_main_widget_list()
+if N_MONITORS == 2:
+    widgets_monitor_1 = init_secondary_widget_list()
+else:
+    widgets_monitor_1 = init_main_widget_list()
+
+BACKGROUND_DIR = os.path.expanduser("~/Pictures/Backgrounds/waves")
+wallpaper_list = random.sample(os.listdir(BACKGROUND_DIR), 2)
+   
+
+screens = [
+    Screen(
+        wallpaper=os.path.join(BACKGROUND_DIR, wallpaper_list[0]),
+        wallpaper_mode="stretch",
+        top=bar.Bar(
+            widgets_monitor_1,
+            23,
+            background = COLORS[0],
+            opacity = 0.85,
+            margin = [4, 10, 0, 10]
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+    ),
+    Screen(
+        wallpaper=os.path.join(BACKGROUND_DIR, wallpaper_list[1]),
+        wallpaper_mode="stretch",
+        top=bar.Bar(
+            widgets_monitor_2,
+            23,
+            background = COLORS[0],
+            opacity = 0.85,
+            margin = [4, 10, 0, 10],
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
