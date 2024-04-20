9453797d314ed9ee7cf7778019aba216d74cd251
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2024-04-20 11:40:36 +0200

    Automated screen turn-off

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-picom &
+
+# cat /proc/acpi/button/lid/LID0/state | grep open -q
+if grep -q "open" /proc/acpi/button/lid/LID0/state ; then
+    echo "Open LID"
+    xrandr --output eDP-1 --auto
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off
+else
+    echo "Closed LID"
+    xrandr --output eDP-1 --off
+fi
+
+# picom &
